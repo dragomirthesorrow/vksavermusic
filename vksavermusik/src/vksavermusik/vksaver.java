94e0bc28341f2ada6b210182b85d9af3e23f669b
dragomirthesorrow <dragomir-onishhenko@yandex.ru>
2016-07-19 11:16:05 +0300

    Planning of project

---
+++ vksavermusik/src/vksavermusik/vksaver.java
@@ -0,0 +1,12 @@
+package vksavermusik;
+
+public class vksaver {
+
+	public static void main(String[] args) {
+		// далее переименование фј…Ћќв и перемещение в созданную папку
+		
+		//creating a folder
+
+	}
+
+}
