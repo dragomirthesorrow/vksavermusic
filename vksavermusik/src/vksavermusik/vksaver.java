14f00a742c2f001a25a630f1392fc83094086f70
dragomirthesorrow <dragomir-onishhenko@yandex.ru>
2016-07-19 11:34:29 +0300

    Creating folder

--- vksavermusik/src/vksavermusik/vksaver.java
+++ vksavermusik/src/vksavermusik/vksaver.java
@@ -1,12 +1,13 @@
 package vksavermusik;
-
+import java.io.File;
 public class vksaver {
 
 	public static void main(String[] args) {
 		// далее переименование фј…Ћќв и перемещение в созданную папку
 		
 		//creating a folder
-
+		file(/sdcrd0/).mkdir(vkmusicmp3);
+		//Renaming and moving files to created path
 	}
 
 }
