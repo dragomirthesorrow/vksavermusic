6478dadbcd8a72b337bcce5894304052ce0dbfce
dragomirthesorrow <dragomir-onishhenko@yandex.ru>
2016-07-19 16:10:45 +0300

    One success full cicle.

--- vksavermusik/src/vksavermusik/vksaver.java
+++ vksavermusik/src/vksavermusik/vksaver.java
@@ -9,12 +9,32 @@ public class vksaver {
 		// далее переименование фј…Ћќв и перемещение в созданную папку
 		
 		//creating a folder
-		//File dir1 = new File("C://users//a.onischenko.DGGRU//desktop//VKmusic");
-		//boolean created = dir1.mkdir();
+		File dir1 = new File("C://users//a.onischenko.DGGRU//desktop//VKmusic");
+		boolean created = dir1.mkdir();
 		
 		//Renaming and moving files to created path
 		String file[] = new File("C://users//a.onischenko.DGGRU//desktop//Music").list();
-		for(int i=0; i<file.length;i++) System.out.println(file[i]);
+		for(int i=0; i<file.length; i++) {
+			//Here began rename and move for each file
+			System.out.println(file[i]);
+			 File f = null;
+		     File f1 = null;
+		     boolean bool = false;
+			try{
+				f = new File("C://users//a.onischenko.DGGRU//desktop//Music//*.amr");
+				f1 = new File("C://users//a.onischenko.DGGRU//desktop//VKmusic//*.mp3");
+				bool = f.renameTo(f1);
+				System.out.println("renamed");
+			}catch(Exception e){
+		         // if any error occurs
+		         e.printStackTrace();
+		      }
+		}
 		
-}
-}
+		//String f = new File("C://users//a.onischenko.DGGRU//desktop//Music").getName();
+		//for(int i=0; i<f.length;i++) {
+			//Here began rename and move for each file
+		//	System.out.println(f[i]);}
+		}}
+		
+
